4590793c561f3fc19508d0c1b7ac464d57ef7f75
alee29304 <alexandermaldonado321.0@gmail.com>
2026-05-04 12:56:52 -0400

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,14 +1,12 @@
 ''' 6- Crear una función que reciba una lista de edades y clasifique a las personas en tres grupos: menores de edad, adultos y adultos mayores (60+).
  Debe mostrar la cantidad de personas en cada grupo. '''
 
-def veces_repetidas():
-    cantidad = int(input("Ingrese la cantidad de edades: "))
-
-    adultos_mayores = 0
-    adultos = 0
+def solicitar_edad(cantidad):
     menores = 0
-    
-    while (cantidad):
+    adultos = 0
+    adultos_mayores = 0
+
+    for i in range(cantidad):
         edad = int(input("Ingrese su edad: "))
         if edad < 18:
             menores += 1
@@ -16,7 +14,13 @@ def veces_repetidas():
             adultos += 1
         else:
             adultos_mayores += 1
-            print(f"Menores: {menores}")
-            print(f"Adultos: {adultos}")
-            print(f"Adultos mayores: {adultos_mayores}")
-veces_repetidas()
+    return menores, adultos, adultos_mayores
+
+def resultado_ejercicio():
+    cantidad = int(input("Ingrese la cantidad de edades: "))
+    menores, adultos, adultos_mayores = solicitar_edad(cantidad)
+    print(f"Menores: {menores}")
+    print(f"Adultos: {adultos}")
+    print(f"Adultos mayores: {adultos_mayores}")
+
+resultado_ejercicio()
